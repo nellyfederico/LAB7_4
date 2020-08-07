f315c39eed653ff0e4f570440362e80661256fec
nellyfederico <nallelyfedericoc@gmail.com>
2020-08-07 18:13:43 -0500

    LAB7_4

---
+++ LAB7_4DatingGames/DatingProfile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB7_4DatingGames
+{
+    class DatingProfile
+        {
+            public string firstname;
+            private string lastname;
+            public int age;
+            public string gender;
+            public string bio;
+            private List<Messages> myMessages;
+
+            public DatingProfile(string firstname, string lastname, int age, string gender)
+            {
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.age = age;
+                this.gender = gender;
+                myMessages = new List<Messages>();
+            }
+
+            public void WriteBio(string text)
+            {
+                bio = text;
+            }
+
+            public void AddToInbox(Messages message)
+            {
+                myMessages.Add(message);
+
+            }
+
+            public void SendMessage(string messageTitle, string messageData, DatingProfile sentTo)
+            {
+                Messages message = new Messages(this, messageTitle, messageData);
+                sentTo.AddToInbox(message);
+            }
+
+            public void ReadMessage()
+            {
+                foreach (Messages message in myMessages)
+                {
+                    if (message.isRead == false)
+                    {
+                        Console.WriteLine(message.messageTitle);
+                        Console.WriteLine(message.messageData);
+                        message.isRead = true;
+                    }
+                }
+            }
+        }
+    }
---
+++ LAB7_4DatingGames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LAB7_4DatingGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile bob = new DatingProfile("Mark", "Slone", 40, "Male");
+            bob.WriteBio("New to this online dating.");
+
+            DatingProfile susie = new DatingProfile("Lexie", "Grey", 25, "Female");
+            susie.WriteBio("Love to read and write.");
+
+            bob.SendMessage("Hello Lexie", "When is the new book coming out?", susie);
+            susie.ReadMessage();
+        }
+    }
+}
